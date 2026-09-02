d82dfec1022bf1fc8884888846624102062cdff3
Dravya Shah <dravyashah12@gmail.com>
2026-09-02 13:49:33 +0530

    Time: 0 ms (100%), Space: 44.6 MB (99.41%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
+        /* if in nums1 all are odd or all are even than in nums2 it will be same bcoz of
+           nums1[i] = num2[i]
+
+           but if let say one is odd and another one is even in nums 1 than difference of two 
+           numbbers will be odd and another number will be odd */
